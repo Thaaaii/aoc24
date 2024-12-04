0984df16d4a1679bff765e2a5513f5972e15d2c6
Thai <ThaiDuongPhamLe02@gmail.com>
2024-12-04 20:38:54 +0100

    add day 4 solutions

--- day4/solution.go
+++ day4/solution.go
@@ -9,9 +9,10 @@ import (
 func Solution() {
 	data := parseData()
 	matrixData := newMatrix(data)
-	sumXmas := matrixData.countXmas()
-	fmt.Println("Day 4, Solution 1 result: ", sumXmas)
-	fmt.Println("Day 4, Solution 2 result: ")
+	sumXMAS := matrixData.countXmas()
+	fmt.Println("Day 4, Solution 1 result: ", sumXMAS)
+	sumX_MAS := matrixData.countCrossXmas()
+	fmt.Println("Day 4, Solution 2 result: ", sumX_MAS)
 }
 
 func parseData() []string {
@@ -47,10 +48,10 @@ func newMatrix(data []string) matrix {
 
 func (m matrix) countXmas() int {
 	sum := 0
-	for _, row := range m.data {
-		for _, char := range row {
+	for i, row := range m.data {
+		for j, char := range row {
 			if string(char) == "X" {
-				sum += m.checkEveryDirection()
+				sum += m.checkEveryDirectionXmas(i, j)
 			}
 		}
 	}
@@ -58,14 +59,139 @@ func (m matrix) countXmas() int {
 	return sum
 }
 
-func (m matrix) checkEveryDirection() int {
+func (m matrix) checkEveryDirectionXmas(i, j int) int {
 	sum := 0
+	wordBuffer := []byte{0, 0, 0, 0}
 
-	// diagonal
+	// diagonal: up right
+	if i-3 >= 0 && j+3 < m.n {
+		wordBuffer[0] = m.data[i][j]
+		wordBuffer[1] = m.data[i-1][j+1]
+		wordBuffer[2] = m.data[i-2][j+2]
+		wordBuffer[3] = m.data[i-3][j+3]
+		if string(wordBuffer) == "XMAS" {
+			sum++
+		}
+	}
 
-	// vertical
+	// diagonal: down right
+	if i+3 < m.m && j+3 < m.n {
+		wordBuffer[0] = m.data[i][j]
+		wordBuffer[1] = m.data[i+1][j+1]
+		wordBuffer[2] = m.data[i+2][j+2]
+		wordBuffer[3] = m.data[i+3][j+3]
+		if string(wordBuffer) == "XMAS" {
+			sum++
+		}
+	}
 
-	// horizontal
+	// diagonal: down left
+	if i+3 < m.m && j-3 >= 0 {
+		wordBuffer[0] = m.data[i][j]
+		wordBuffer[1] = m.data[i+1][j-1]
+		wordBuffer[2] = m.data[i+2][j-2]
+		wordBuffer[3] = m.data[i+3][j-3]
+		if string(wordBuffer) == "XMAS" {
+			sum++
+		}
+	}
+
+	// diagonal: up left
+	if i-3 >= 0 && j-3 >= 0 {
+		wordBuffer[0] = m.data[i][j]
+		wordBuffer[1] = m.data[i-1][j-1]
+		wordBuffer[2] = m.data[i-2][j-2]
+		wordBuffer[3] = m.data[i-3][j-3]
+		if string(wordBuffer) == "XMAS" {
+			sum++
+		}
+	}
+
+	// vertical: left
+	if j-3 >= 0 {
+		wordBuffer[0] = m.data[i][j]
+		wordBuffer[1] = m.data[i][j-1]
+		wordBuffer[2] = m.data[i][j-2]
+		wordBuffer[3] = m.data[i][j-3]
+		if string(wordBuffer) == "XMAS" {
+			sum++
+		}
+	}
+
+	// vertical: right
+	if j+3 < m.n {
+		wordBuffer[0] = m.data[i][j]
+		wordBuffer[1] = m.data[i][j+1]
+		wordBuffer[2] = m.data[i][j+2]
+		wordBuffer[3] = m.data[i][j+3]
+		if string(wordBuffer) == "XMAS" {
+			sum++
+		}
+	}
+
+	// horizontal: up
+	if i-3 >= 0 {
+		wordBuffer[0] = m.data[i][j]
+		wordBuffer[1] = m.data[i-1][j]
+		wordBuffer[2] = m.data[i-2][j]
+		wordBuffer[3] = m.data[i-3][j]
+		if string(wordBuffer) == "XMAS" {
+			sum++
+		}
+	}
+
+	// horizontal: down
+	if i+3 < m.m {
+		wordBuffer[0] = m.data[i][j]
+		wordBuffer[1] = m.data[i+1][j]
+		wordBuffer[2] = m.data[i+2][j]
+		wordBuffer[3] = m.data[i+3][j]
+		if string(wordBuffer) == "XMAS" {
+			sum++
+		}
+	}
 
 	return sum
+}
+
+func (m matrix) countCrossXmas() int {
+	sum := 0
+	for i, row := range m.data {
+		for j, char := range row {
+			if string(char) == "A" {
+				sum += m.checkEveryDirectionCrossXmas(i, j)
+			}
+		}
+	}
+
+	return sum
+}
+
+func (m matrix) checkEveryDirectionCrossXmas(i, j int) int {
+	sum := 0
+	wordBuffer := []byte{0, 0, 0}
+
+	if i-1 >= 0 && j-1 >= 0 && i+1 < m.m && j+1 < m.n {
+		// diagonal: right up-left down
+		wordBuffer[0] = m.data[i-1][j+1]
+		wordBuffer[1] = m.data[i][j]
+		wordBuffer[2] = m.data[i+1][j-1]
+		if string(wordBuffer) == "MAS" || string(wordBuffer) == "SAM" {
+			sum++
+		}
+
+		// diagonal: left up-right down
+		wordBuffer[0] = m.data[i-1][j-1]
+		wordBuffer[1] = m.data[i][j]
+		wordBuffer[2] = m.data[i+1][j+1]
+		if string(wordBuffer) == "MAS" || string(wordBuffer) == "SAM" {
+			sum++
+		}
+	}
+
+	if sum == 2 {
+		return 1
+	}
+
+	return 0
 }
